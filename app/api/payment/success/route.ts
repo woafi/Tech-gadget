03d7d7f4d5145a03597f83afd1496e354e7ca3c9
Mohammad Woafi <woafisun@yahoo.com>
2026-07-01 23:57:16 +0600

    feat: update payment success URLs to new endpoint and remove obsolete route

--- app/api/payment/success/route.ts
+++ app/api/payment/success/route.ts
@@ -55,12 +55,8 @@ export async function POST(request: NextRequest) {
     const transactionId = request.nextUrl.searchParams.get("tran_id");
     const orderId = request.nextUrl.searchParams.get("order_id");
 
-    return NextResponse.redirect(`${process.env.CLIENT_URL}/paymentSSLsuccess?tran_id=${transactionId}&order_id=${orderId}`);
-    // return NextResponse.redirect(`${process.env.CLIENT_URL}/paymentSuccess?tran_id=${transactionId}&order_id=${orderId}`);
-}
-export async function GET(request: NextRequest) {
-    const transactionId = request.nextUrl.searchParams.get("tran_id");
-    const orderId = request.nextUrl.searchParams.get("order_id");
-
-    return NextResponse.redirect(`${process.env.CLIENT_URL}/paymentSSLsuccess?tran_id=${transactionId}&order_id=${orderId}`);
+    return NextResponse.redirect(
+  `${process.env.CLIENT_URL}/paymentSuccess?tran_id=${transactionId}&order_id=${orderId}`,
+  303
+);
 }
